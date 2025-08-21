85e8272c16e5b3b6f6af0178818da1afe1b93113
Chris Lightner <chrislightner@Lightner-MacBook-Pro.local>
2025-08-21 09:51:14 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# ADA Insurance Landing Page
+
+This site is hosted at Heroku.
+
+---
+
+## When committing changes, please make two pushes.
+
+### THIS HAS BEEN MIGRATED TO GITHUB WORKFLOW
+
+``git push``  
+ or  
+``git push origin master``
+
+
+---
+
+## This site doesn't actually use PHP
+
+In order to "trick" Heroku into serving this static site (it is not an app), we pretend it's a php app. We do this with two files:
+
+``composer.json`` Although this file is basically empty, it tells Heroku that this is a php app and to look for an index.php file
+
+``index.php`` There is no php code to execute, but Heroku will be looking for an index.php page - so we use this as our home page
+
+---
+
+## Redeploy to Heroku 20 10/18/21
---
+++ index.php
@@ -0,0 +1,184 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
+
+  <title>ADA Insurance</title>
+
+  <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/album/">
+
+  <!-- Bootstrap core CSS -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <!-- Custom styles for this template -->
+  <link href="css/styles.css" rel="stylesheet">
+</head>
+
+<body>
+
+  <header>
+    <div class="navbar navbar-dark bg-dark box-shadow p-3">
+      <div class="container d-flex justify-content-between">
+        <a href="#" class="navbar-brand d-flex align-items-center">
+          <!-- <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="mr-2"><path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"></path><circle cx="12" cy="13" r="4"></circle></svg>
+            <strong>Album</strong> -->
+          <img src="images/ADA_Insurance_PoweredbyAlliant_Horiz_Rev.svg" alt="ADA Insurance Powered by Alliant Logo"
+            width="200" style="width:200px;display:block;">
+        </a>
+        <!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button> -->
+      </div>
+    </div>
+  </header>
+
+  <main role="main">
+
+    <section class="jumbotron text-center">
+      <div class="jumbotron-bg-video-wrapper">
+        <video autoplay muted loop playsinline poster="">
+          <!-- <source src="video/istockphoto-1161984646-640_adpp_is.mp4" type="video/mp4"> -->
+          <!-- <source src="video/istockphoto-1811601734-640_adpp_is.mp4" type="video/mp4"> -->
+          <!-- <source src="video/istockphoto-2153479897-640_adpp_is.mp4" type="video/mp4"> -->
+          <source src="video/istockphoto-2155395160-640_adpp_is.mp4" type="video/mp4">
+          Your browser does not support the video tag.
+        </video>
+      </div>
+      <div class="container jumbotron-content">
+        <h1 class="jumbotron-heading">ADA&nbsp;Insurance Powered&nbsp;by&nbsp;Alliant</h1>
+        <h2 class="pt-1">Designed&nbsp;for&nbsp;dentists. Endorsed&nbsp;by&nbsp;the&nbsp;ADA.</h2>
+        <!-- <p class="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don't simply skip over it entirely.</p>
+          <p>
+            <a href="#" class="btn btn-primary my-2">Main call to action</a>
+            <a href="#" class="btn btn-secondary my-2">Secondary action</a>
+          </p> -->
+      </div>
+    </section>
+
+    <div class="py-5 bg-light leadin">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-md-12 text-center">
+            The American Dental Association has partnered with Alliant Insurance Services to offer ADA members enhanced insurance solutions – helping you protect yourself, your loved ones, and your practice.
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <div class="pt-5 pb-4 bg-light">
+      <div class="container">
+
+        <div class="row">
+          <div class="col-md-4">
+            <div class="card mb-4">
+              <img class="card-img-top" src="images/icon-tooth.png" alt="icon-tooth">
+              <div class="card-body">
+                <p class="card-text"><strong>Supported by the ADA</strong></p>
+                <p class="card-text">The only insurance coverage endorsed by the ADA, tailored specifically for dentists and dental&nbsp;students.
+                </p>
+                <!-- <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                      <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                    </div>
+                    <small class="text-muted">9 mins</small>
+                  </div> -->
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card mb-4">
+              <img class="card-img-top" src="images/icon-service.png" alt="icon-service">
+              <div class="card-body">
+                <p class="card-text"><strong>Personalized Guidance</strong></p>
+                <p class="card-text">Licensed Alliant representatives can help you navigate your options and find coverage that fits your&nbsp;needs.
+                </p>
+                <!-- <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                      <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                    </div>
+                    <small class="text-muted">9 mins</small>
+                  </div> -->
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card mb-4">
+              <img class="card-img-top" src="images/icon-stages.png" alt="icon-stages">
+              <div class="card-body">
+                <p class="card-text"><strong>Comprehensive Solutions</strong></p>
+                <p class="card-text">From personal to professional, this insurance helps protect you at every stage of
+                  your career and&nbsp;life.
+                </p>
+                <!-- <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                      <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                    </div>
+                    <small class="text-muted">9 mins</small>
+                  </div> -->
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="pb-5 bg-light callout">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-md-6 box p-3 rounded box-shadow">
+
+            <div class="container">
+              <div class="row">
+                <div class="col-md-3 d-flex align-items-center justify-content-center">
+                    <p style="font-size: 4rem; color: #000000;font-weight: bold;">!</p>
+                </div>
+                <div class="col-md-9 d-flex align-items-center">
+                  <p><strong>Stay tuned for plan updates and announcements exclusively for ADA&nbsp;members.</strong><br>Have questions today? Call us at [insert phone number]. Our team is ready to&nbsp;help!</p>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </main>
+
+  <footer class="text-muted">
+    <div class="container">
+      <!-- <p class="float-right">
+          <a href="#">Back to top</a>
+        </p>
+        <p>example is &copy; Bootstrap, but please download and customize it for yourself!</p>
+        <p>New to Bootstrap? <a href="../../">Visit the homepage</a> or read our <a href="../../getting-started/">getting started guide</a>.</p> -->
+      <p>© ADA Insurance | Powered by Alliant</p>
+    </div>
+  </footer>
+
+  <!-- Bootstrap core JavaScript
+    ================================================== -->
+  <!-- Placed at the end of the document so the pages load faster -->
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+    integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+  </script>
+  <!-- <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script> -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.11.8/umd/popper.min.js"
+    integrity="sha512-TPh2Oxlg1zp+kz3nFA0C5vVC6leG/6mm1z9+mA81MI5eaUVqasPLO8Cuk4gMF4gUfP5etR73rgU/8PNMsSesoQ=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+  </script>
+  <script src="../../assets/js/vendor/holder.min.js"></script>
+</body>
+
+</html>
